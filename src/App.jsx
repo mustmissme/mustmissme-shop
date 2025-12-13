f0352b6e1d3bb83f8f8cecccc8f17d9c4ad04980
mustmissme <natdanai3223@gmail.com>
2025-12-14 03:59:37 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -72,7 +72,7 @@ const CONTACT_LINKS = {
   tiktok:
     "https://www.tiktok.com/@mustmissme?_t=ZS-8zYkNa7Cxmq&_r=1",
   shopee:
-    "https://shopee.co.th/reviewwwwwwwwww?uls_trackid=547g3fct004i&utm_campaign=-&utm_content=-&utm_medium=affiliates&utm_source=an_15359450009&utm_term=dz7vodofwim5",
+    "https://s.shopee.co.th/AA7aJvl9qD",
   line:
     "https://line.me/R/ti/p/@078vlxgl?ts=09091148&oat_content=url",
 };
